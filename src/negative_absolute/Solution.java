10cc5954749214727b01bd65fc75a7bc29ab8c33
Sergios Kolios <skolios@gmail.com>
2018-06-17 22:59:47 +0200

    Code Cleanup Absolute Negative Integer - Moving to it's own package

--- src/negative_absolute/Solution.java
+++ src/negative_absolute/Solution.java
@@ -1,16 +1,18 @@
-package com.hackerrank.elastictest.test02;
+package negative_absolute;
 
 public class Solution {
 
-    static int thinkPositive(int i) {
+    private static int thinkPositive(int i) {
         if (i == Integer.MIN_VALUE) {
             i++;
             return Math.abs(i);
         }
-        return Math.abs(i);  // This doesn't always return a *positive* number.
+        return Math.abs(i);
+        // This doesn't always return a *positive* number.
         /* Yes, we know changing this to `return 1;` will pass all the tests.
          * While cute that isn't quite what we're looking for. See the javadoc
-         * our reasoning. */
+         * our reasoning.
+         * */
     }
 
     public static void main(String[] args) {
